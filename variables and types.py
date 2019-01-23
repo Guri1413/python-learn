1945a2af3619c23db69e8b19a299a23db8ea00f9
Guri1413 <ggrewal1413@gmail.com>
2019-01-22 23:24:54 -0500

    Add files via upload

---
+++ variables and types.py
@@ -0,0 +1,27 @@
+# declaring datatypes
+# declaring int
+myInt=1;
+print(myInt);
+#declaring float
+myFloat=1.0;
+print(myFloat);
+#declaring my float type 2
+myFloat2=float(1);
+print(myFloat2);
+#declaring strings with single comma's
+myString='this is string declaring';
+print(myString);
+#declaring strings with double comma's
+myString2="this is string declaring";
+print(myString2);
+#operators on numbers
+firstnum=1;
+secondnum=2;
+thirdnum=firstnum+secondnum;
+print(thirdnum);
+
+#operators on strings
+firststring="hello";
+secondstring="people";
+thirdstring=firststring+" "+secondstring;
+print(thirdstring);
